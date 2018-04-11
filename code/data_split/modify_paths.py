3ca07340bfdcf0cc9589089c306cbee98ba66a52
sorodoc <sodoroc@gmail.com>
2018-04-11 19:27:55 +0200

    clean residual files

--- code/data_split/modify_paths.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == '__main__':
-    fin = open('test_ids.txt', 'r')
-    fout = open('test_ids2.txt', 'w')
-    for line in fin:
-        l = line.strip().split('/')
-        fout.write('/'.join(l[-3:]) + '\n')
-
